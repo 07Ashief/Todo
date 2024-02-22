80693b71b7d49feeca1f15f2e40942ebce9470d6
Ashief <07ashief@gmail.com.com>
2024-02-22 16:33:05 +0530

    second commit

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
-let todoList = [];
+let todoList = [
+  { item: "Buy Milk", dueDate: "4/10/2023" },
+  { item: "Go to college", dueDate: "4/10/2023" },
+];
 displayItems();
 
 function addTodo() {
-    let inputElement = document.querySelector('#todo-input')
-    let todoItem = inputElement.value;
-    todoList.push(todoItem);
-    inputElement.value = '';
-    displayItems();
-    
+  let inputElement = document.querySelector("#todo-input");
+  let dateElement = document.querySelector("#todo-date");
+  let todoItem = inputElement.value;
+  let todoDate = dateElement.value;
+  todoList.push({ item: todoItem, dueDate: todoDate });
+  inputElement.value = "";
+  dateElement.value = "";
+  displayItems();
 }
 
-function displayItems(){
-    let displayElement = document.querySelector('#todo-items')
-    displayElement.innerText = todoList + '\n'
-    // displayElement = ''
+function displayItems() {
+  let containerElement = document.querySelector(".todo-container");
 
-    // for(let i=0; i < todoList.length; i++ ){
-    //     displayElement.innerText = displayElement.innerText + '/n' + todoList[i];
-        
-        
+  let newHtml = "";
 
-    // }
-
-
+  for (i = 0; i < todoList.length; i++) {
+    // let item = todoList[i].item;
+    // let dueDate = todoList[i].dueDate;
+    let {item, dueDate} = todoList[i];
+    newHtml += `
+        <div>
+            <span>${item}</span>
+            <span>${dueDate}</span>
+            <button onclick = 'todoList.splice(${i}, 1); displayItems();'>Delete</button>
+        </div>
+        `;
+  }
+  containerElement.innerHTML = newHtml;
 }
